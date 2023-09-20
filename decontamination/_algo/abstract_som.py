60e095b5bf8bc30029ac18273367f9310940365b
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2023-09-20 19:45:05 +0200

    update online and batch som

--- decontamination/_algo/abstract_som.py
+++ decontamination/_algo/abstract_som.py
@@ -417,7 +417,7 @@ def _find_bmu_xpu(weights: np.ndarray, vector: np.ndarray, mn: int) -> int:
 
     for index in range(mn):
 
-        distance = np.linalg.norm(np.subtract(weights[index], vector))
+        distance = np.linalg.norm(weights[index] - vector)
 
         if min_distance > distance:
 

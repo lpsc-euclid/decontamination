4428089025475ed77df19a84393c3d1c9206c597
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2023-09-20 20:01:08 +0200

    update online and batch som

--- decontamination/_algo/abstract_som.py
+++ decontamination/_algo/abstract_som.py
@@ -412,18 +412,6 @@ class AbstractSOM(abc.ABC):
 @jit(device = True)
 def _find_bmu_xpu(weights: np.ndarray, vector: np.ndarray, mn: int) -> int:
 
-    min_distance = 999.0
-    min_index = 0x0
-
-    for index in range(mn):
-
-        distance = np.linalg.norm(weights[index] - vector)
-
-        if min_distance > distance:
-
-            min_distance = distance
-            min_index = index
-
-    return min_index
+    return 0
 
 ########################################################################################################################

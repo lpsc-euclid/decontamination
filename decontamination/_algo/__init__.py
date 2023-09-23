9c1c58e8fe15fa24aaec6796301ad750601f31f7
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2023-09-23 23:44:31 +0200

    update jit and abstract som

--- decontamination/_algo/__init__.py
+++ decontamination/_algo/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 ########################################################################################################################
+
 import typing
 
 import numba as nb
@@ -15,16 +16,24 @@ def dataset_to_generator_builder(dataset: typing.Union[np.ndarray, typing.Callab
 
 def batch_iterator(vectors: np.ndarray, n_chunks: int) -> typing.Iterator[np.ndarray]:
 
-    size = len(vectors)
+    ####################################################################################################################
 
-    chunk_size = size // n_chunks
+    chunk_size, chunk_remaining = divmod(vectors.shape[0], n_chunks)
+
+    ####################################################################################################################
 
     for i in range(n_chunks):
 
-        s = i * chunk_size #############################
-        e = s + chunk_size if i < n_chunks - 1 else size
+        s = i * chunk_size
+        e = s + chunk_size
 
         yield vectors[s: e]
+
+    ####################################################################################################################
+
+    if chunk_remaining > 0:
+
+        yield vectors[n_chunks * chunk_size: ]
 
 ########################################################################################################################
 

46eb54fbe49c8cac284ee19324f91faccd386d55
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2023-10-02 14:08:37 +0200

    batch som implementation + online som optimizations

--- decontamination/_algo/som_batch.py
+++ decontamination/_algo/som_batch.py
@@ -229,12 +229,12 @@ def _train_xpu(numerator: np.ndarray, denominator: np.ndarray, quantization_erro
     # UPDATE ERRORS                                                                                                    #
     ####################################################################################################################
 
-    #if np.sum((bmu1 - bmu2) ** 2) > penalty_dist:
+    if np.sum((bmu1 - bmu2) ** 2) > penalty_dist:
 
-        #quantization_errors[err_bin] += math.sqrt(min_distance1)
-        #topographic_errors[err_bin] += 1.0000000000000000000000
+        quantization_errors[err_bin] += math.sqrt(min_distance1)
+        topographic_errors[err_bin] += 1.0000000000000000000000
 
-    #quantization_errors[err_bin] += math.sqrt(min_distance1)
-    #topographic_errors[err_bin] += 0.0000000000000000000000
+    quantization_errors[err_bin] += math.sqrt(min_distance1)
+    topographic_errors[err_bin] += 0.0000000000000000000000
 
 ########################################################################################################################

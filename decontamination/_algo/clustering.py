d2c95526696cbef79e377f6be281f43bfb2f7da1
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2023-10-07 11:13:18 +0200

    update clustering

--- decontamination/_algo/clustering.py
+++ decontamination/_algo/clustering.py
@@ -2,12 +2,17 @@
 ########################################################################################################################
 
 import math
+import timeit
 
 import numpy as np
 import numba as nb
 import numba.cuda as cu
 
+from scipy.spatial import distance
+from scipy.cluster import hierarchy
+
 from .. import jit, device_array_full
+
 
 ########################################################################################################################
 
@@ -58,7 +63,7 @@ class Clustering(object):
     ####################################################################################################################
 
     @staticmethod
-    def clusterize(vectors: np.ndarray, n_clusters: int, enable_gpu: bool = True, threads_per_blocks: int = 32) -> np.ndarray:
+    def clusterize2(vectors: np.ndarray, n_clusters: int, enable_gpu: bool = True, threads_per_blocks: int = 32) -> np.ndarray:
 
         """
         Clusters the input array using the Lance-Williams hierarchical clustering with complete-linkage algorithm.
@@ -88,7 +93,9 @@ class Clustering(object):
         # COMPUTE DISTANCES                                                                                            #
         ################################################################################################################
 
+        time1 = timeit.default_timer()
         distances = Clustering._init_distances(vectors, enable_gpu = enable_gpu, threads_per_blocks = threads_per_blocks)
+        time2 = timeit.default_timer()
 
         ################################################################################################################
         # COMPUTE CLUSTERS                                                                                             #
@@ -105,6 +112,10 @@ class Clustering(object):
         ################################################################################################################
 
         result[nan_mask] = -1
+
+        time3 = timeit.default_timer()
+
+        print(time2 - time1, time3 - time2)
 
         ################################################################################################################
         # REINDEX CLUSTERS                                                                                             #
@@ -126,6 +137,81 @@ class Clustering(object):
                     cnt += 1
 
         ################################################################################################################
+
+        return result
+
+    ####################################################################################################################
+
+    @staticmethod
+    def clusterize(vectors: np.ndarray, n_clusters: int) -> np.ndarray:
+        """
+        Clusters the input array using the Lance-Williams hierarchical clustering with complete-linkage algorithm.
+
+        Parameters
+        ----------
+        vectors : np.ndarray
+            Flat input array to be clustered.
+        n_clusters : int
+            Desired number of clusters.
+
+        Return
+        ------
+        Array giving a cluster identifier for each input vector.
+        """
+
+        time1 = timeit.default_timer()
+
+        ################################################################################################################
+
+        nan_mask = np.any(np.isnan(vectors), axis = -1)
+
+        ################################################################################################################
+        # COMPUTE DISTANCES                                                                                            #
+        ################################################################################################################
+
+        distances = distance.pdist(vectors[~nan_mask])
+
+        ################################################################################################################
+        # COMPUTE CLUSTERS                                                                                             #
+        ################################################################################################################
+
+        z = hierarchy.linkage(distances, method = 'complete')
+
+        cluster_ids = hierarchy.fcluster(z, n_clusters, criterion = 'maxclust')
+
+        ################################################################################################################
+        # REINDEX CLUSTERS                                                                                             #
+        ################################################################################################################
+
+        j = 0
+        k = 0
+
+        cluster_dict = {}
+
+        result = np.full(vectors.shape[0], -1, dtype = int)
+
+        ################################################################################################################
+
+        for i in range(vectors.shape[0]):
+
+            if not nan_mask[i]:
+
+                cluster_id = cluster_ids[j]
+
+                if cluster_id in cluster_dict:
+                    result[i] = cluster_dict[cluster_id] # k
+                else:
+                    result[i] = cluster_dict[cluster_id] = k
+
+                    k += 1
+
+                j += 1
+
+        ################################################################################################################
+
+        time2 = timeit.default_timer()
+
+        print(time2 - time1)
 
         return result
 

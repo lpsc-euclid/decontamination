fc1be39d6ad1d1aff0b2008fc4e93a2f4a594ea3
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2023-10-11 08:32:08 +0200

    galaxy generation

--- decontamination/generator/generator_from_density.py
+++ decontamination/generator/generator_from_density.py
@@ -5,7 +5,6 @@ import math
 import typing
 
 import numpy as np
-import numba as nb
 
 from . import xy2thetaphi, get_cell_size, generator_abstract
 
@@ -51,41 +50,34 @@ class Generator_FromDensity(generator_abstract.Generator_Abstract):
 
         ################################################################################################################
 
+        rng = np.random.default_rng(seed = seed)
+
+        ################################################################################################################
+
         return Generator_FromDensity._generate(
+            rng,
             self._nside,
             self._x_diamonds,
             self._y_diamonds,
             density_map,
-            mult_factor,
-            seed
+            mult_factor
         )
 
     ####################################################################################################################
 
     @staticmethod
-    @nb.njit(fastmath = True, parallel = True)
-    def _generate(nside: int, x_diamonds: np.ndarray, y_diamonds: np.ndarray, density_map: np.ndarray, mult_factor: float, seed: typing.Optional[int]) -> typing.Tuple[np.ndarray, np.ndarray]:
+    def _generate(rng: np.random.Generator, nside: int, x_diamonds: np.ndarray, y_diamonds: np.ndarray, density_map: np.ndarray, mult_factor: float) -> typing.Tuple[np.ndarray, np.ndarray]:
 
         ################################################################################################################
-
-        np.random.seed(seed)
-
+        #                                                                                                              #
         ################################################################################################################
 
-        n_galaxies_per_pixels = np.empty_like(density_map, dtype = np.float32)
+        n_galaxies_per_pixels = rng.poisson(mult_factor * density_map)
 
-        n_total_galaxies = 0x00000000000000000000000000000000000000000000
+        n_total_galaxies = np.sum(n_galaxies_per_pixels)
 
         ################################################################################################################
-
-        for i in range(density_map.shape[0]):
-
-            n_galaxies = np.random.poisson(mult_factor * density_map[i])
-
-            n_galaxies_per_pixels[i] = n_galaxies
-
-            n_total_galaxies += n_galaxies
-
+        #                                                                                                              #
         ################################################################################################################
 
         # HEALPix diamonds to squares -> +45° rotation.
@@ -102,14 +94,11 @@ class Generator_FromDensity(generator_abstract.Generator_Abstract):
         x_galaxies = np.empty(n_total_galaxies, dtype = np.float32)
         y_galaxies = np.empty(n_total_galaxies, dtype = np.float32)
 
-        for i in nb.prange(n_galaxies_per_pixels.shape[0]):
+        for i, n_galaxies in enumerate(n_galaxies_per_pixels):
 
-            np.random.seed(seed + i)
+            end_idx = start_idx + n_galaxies
 
-            end_idx = start_idx + n_galaxies_per_pixels[i]
-
-            dx = np.random.uniform(-0.5, +0.5, size = (n_galaxies_per_pixels[i], ))
-            dy = np.random.uniform(-0.5, +0.5, size = (n_galaxies_per_pixels[i], ))
+            dx, dy = rng.uniform(-0.5, +0.5, size = (2, n_galaxies))
 
             x_galaxies[start_idx: end_idx] = x_center[i] + dx * cell_size
             y_galaxies[start_idx: end_idx] = y_center[i] + dy * cell_size
@@ -123,6 +112,8 @@ class Generator_FromDensity(generator_abstract.Generator_Abstract):
         x_galaxies2 = (+x_galaxies + y_galaxies) / math.sqrt(2)
         y_galaxies2 = (-x_galaxies + y_galaxies) / math.sqrt(2)
 
+        ################################################################################################################
+        #                                                                                                              #
         ################################################################################################################
 
         theta, phi, = xy2thetaphi(x_galaxies2, y_galaxies2)

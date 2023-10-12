8f9af7a4ebbb8537b23d123d3a88cdb18271bcc3
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2023-10-12 23:22:19 +0200

    healpix plotting

--- decontamination/plotting/healpix_map.py
+++ decontamination/plotting/healpix_map.py
@@ -7,6 +7,8 @@ import numpy as np
 
 import healpy as hp
 
+import matplotlib.pyplot as plt
+
 ########################################################################################################################
 
 def _catalog_to_density(nside: int, footprint: np.ndarray, sky: np.ndarray, lon: np.ndarray, lat: np.ndarray, nest: bool) -> None:
@@ -23,12 +25,16 @@ def _catalog_to_density(nside: int, footprint: np.ndarray, sky: np.ndarray, lon:
 
 ########################################################################################################################
 
-def _display(nside: int, footprint: np.ndarray, sky: np.ndarray, nest: bool = True, cmap: str = 'jet', norm: typing.Optional[str] = None, v_min: float = None, v_max: float = None, title ='') -> np.ndarray:
+def _get_bounding_box(nside: int, footprint: np.ndarray, nest: bool) -> typing.Tuple[float, float, float, float]:
 
     ####################################################################################################################
+    #                                                                                                                  #
+    ####################################################################################################################
 
-    lon, lat = hp.pix2ang(nside, footprint, nest = nest, lonlat = True)
+    lon, lat = hp.pix2ang(nside, footprint, nest, lonlat = True)
 
+    ####################################################################################################################
+    #                                                                                                                  #
     ####################################################################################################################
 
     lon %= 360
@@ -47,54 +53,98 @@ def _display(nside: int, footprint: np.ndarray, sky: np.ndarray, nest: bool = Tr
 
     ####################################################################################################################
 
-    lon_min = (lon_center + np.min(d_lon) + 360) % 360
-    lon_max = (lon_center + np.max(d_lon) + 360) % 360
-
-    lat_min = np.min(lat)
-    lat_max = np.max(lat)
-
-    ####################################################################################################################
-
-    return hp.cartview(
-        sky,
-        nest = nest,
-        cmap = cmap,
-        norm = norm,
-        min = v_min,
-        max = v_max,
-        lonra = [lon_min, lon_max],
-        latra = [lat_min, lat_max],
-        title = title,
-        return_projected_map = True
+    return (
+        (lon_center + np.min(d_lon) + 360) % 360,
+        (lon_center + np.max(d_lon) + 360) % 360,
+        np.min(lat),
+        np.max(lat),
     )
 
 ########################################################################################################################
 
-def display_healpix(nside: int, pixels: np.ndarray, weights: np.ndarray, nest: bool = True, cmap: str = 'jet', norm: typing.Optional[str] = None, v_min: float = None, v_max: float = None, title: str = '') -> np.ndarray:
+def _display(nside: int, footprint: np.ndarray, sky: np.ndarray, nest: bool = True, cmap: str = 'jet', norm: typing.Optional[str] = None, v_min: float = None, v_max: float = None) -> typing.Tuple[plt.Figure, plt.Axes]:
+
+    ####################################################################################################################
+    #                                                                                                                  #
+    ####################################################################################################################
+
+    lon_min, lon_max, lat_min, lat_max = _get_bounding_box(nside, footprint, nest)
+
+    ####################################################################################################################
+    #                                                                                                                  #
+    ####################################################################################################################
+
+    fig, ax = plt.subplots()
+
+    ####################################################################################################################
+
+    cmap = plt.get_cmap(cmap)
+
+    cmap.set_bad(color = '#808080')
+
+    ####################################################################################################################
+
+    projected_map = hp.cartview(
+        sky,
+        nest = nest,
+        norm = norm,
+        lonra = [lon_min, lon_max],
+        latra = [lat_min, lat_max],
+        return_projected_map = True
+    )
+
+    ####################################################################################################################
+
+    image = ax.imshow(
+        projected_map,
+        cmap = cmap,
+        vmin = v_min,
+        vmax = v_max,
+        aspect = 'auto',
+        origin = 'lower',
+        extent = [
+            lon_min, lon_max,
+            lat_min, lat_max,
+        ]
+    )
+
+    ####################################################################################################################
+
+    fig.colorbar(image, ax = ax)
+
+    ####################################################################################################################
+
+    fig.tight_layout()
+
+    ####################################################################################################################
+
+    return fig, ax
+
+########################################################################################################################
+
+def display_healpix(nside: int, pixels: np.ndarray, weights: np.ndarray, nest: bool = True, cmap: str = 'jet', norm: typing.Optional[str] = None, v_min: float = None, v_max: float = None) -> typing.Tuple[plt.Figure, plt.Axes]:
 
     """
     Displays a HEALPix map.
 
     Parameters
     ----------
-        nside : int
-            The HEALPix nside parameter.
-        pixels : np.ndarray
-            Array of HEALPix pixels.
-        weights : np.ndarray
-            Array of HEALPix weights.
-        nest : bool
-            If **True**, ordering scheme is *NESTED* (default: **True**).
-        cmap : str
-            Color map (default: **'jet'**).
-        norm : typing.Optional[str]
-            Color normalization, hist = histogram equalized color mapping, log = logarithmic color mapping (default: **None**).
-        v_min : float
-            Minimum color scale (default: **None**, uses: min(data)).
-        v_max : float
-            Maximum color scale (default: **None**, uses: max(data)).
-        title : str
-            ???
+    nside : int
+        The HEALPix nside parameter.
+    pixels : np.ndarray
+        Array of HEALPix pixels.
+    weights : np.ndarray
+        Array of HEALPix weights.
+    nest : bool
+        If **True**, ordering scheme is *NESTED* (default: **True**).
+    cmap : str
+        Color map (default: **'jet'**).
+    norm : typing.Optional[str]
+        Color normalization, hist = histogram equalized color mapping, log = logarithmic color mapping (default: **None**).
+    v_min : float
+        Minimum color scale (default: **None**, uses: min(data)).
+    v_max : float
+        Maximum color scale (default: **None**, uses: max(data)).
     """
 
     ####################################################################################################################
@@ -117,39 +167,36 @@ def display_healpix(nside: int, pixels: np.ndarray, weights: np.ndarray, nest: b
         cmap = cmap,
         norm = norm,
         v_min = v_min,
-        v_max = v_max,
-        title = title
+        v_max = v_max
     )
 
 ########################################################################################################################
 
-def display_catalog(nside: int, pixels: np.ndarray, lon: np.ndarray, lat: np.ndarray, nest: bool = True, cmap: str = 'jet', norm: typing.Optional[str] = 'hist', v_min: float = None, v_max: float = None, title: str = '') -> np.ndarray:
+def display_catalog(nside: int, pixels: np.ndarray, lon: np.ndarray, lat: np.ndarray, nest: bool = True, cmap: str = 'jet', norm: typing.Optional[str] = 'hist', v_min: float = None, v_max: float = None) -> typing.Tuple[plt.Figure, plt.Axes]:
 
     """
     Displays a catalog.
 
     Parameters
     ----------
-        nside : int
-            The HEALPix nside parameter.
-        pixels : np.ndarray
-            Array of HEALPix pixels.
-        lon: np.ndarray
-            Array of longitudes.
-        lat: np.ndarray
-            Array of latitudes.
-        nest : bool
-            If **True**, ordering scheme is *NESTED* (default: **True**).
-        cmap : str
-            Color map (default: **'jet'**).
-        norm : typing.Optional[str]
-            Color normalization, hist = histogram equalized color mapping, log = logarithmic color mapping (default: **'hist'**).
-        v_min : float
-            Minimum color scale (default: **None**, uses: min(data)).
-        v_max : float
-            Maximum color scale (default: **None**, uses: max(data)).
-        title : str
-            ???
+    nside : int
+        The HEALPix nside parameter.
+    pixels : np.ndarray
+        Array of HEALPix pixels.
+    lon : np.ndarray
+        Array of longitudes.
+    lat : np.ndarray
+        Array of latitudes.
+    nest : bool
+        If **True**, ordering scheme is *NESTED* (default: **True**).
+    cmap : str
+        Color map (default: **'jet'**).
+    norm : typing.Optional[str]
+        Color normalization, hist = histogram equalized color mapping, log = logarithmic color mapping (default: **'hist'**).
+    v_min : float
+        Minimum color scale (default: **None**, uses: min(data)).
+    v_max : float
+        Maximum color scale (default: **None**, uses: max(data)).
     """
 
     ####################################################################################################################
@@ -172,8 +219,7 @@ def display_catalog(nside: int, pixels: np.ndarray, lon: np.ndarray, lat: np.nda
         cmap = cmap,
         norm = norm,
         v_min = v_min,
-        v_max = v_max,
-        title = title
+        v_max = v_max
     )
 
 ########################################################################################################################

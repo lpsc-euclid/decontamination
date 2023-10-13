faa082b639f571c6ea11d7c51c4e9bf27c5e2c88
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2023-10-13 08:50:38 +0200

    healpix plotting

--- decontamination/plotting/healpix_map.py
+++ decontamination/plotting/healpix_map.py
@@ -86,9 +86,7 @@ def _display(nside: int, footprint: np.ndarray, sky: np.ndarray, nest: bool = Tr
         min = v_min,
         max = v_max,
         lonra = [lon_min, lon_max],
-        latra = [lat_min, lat_max],
-        hold = True,
-        reuse_axes = True
+        latra = [lat_min, lat_max]
     )
 
     ####################################################################################################################

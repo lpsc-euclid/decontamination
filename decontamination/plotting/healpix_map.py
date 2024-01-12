6f33cb3cbb5d03cab32aa371d95467696cd5ef15
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2024-01-12 17:15:10 +0100

    improving correlations and documentation

--- decontamination/plotting/healpix_map.py
+++ decontamination/plotting/healpix_map.py
@@ -54,7 +54,9 @@ def _display(nside: int, footprint: np.ndarray, sky: np.ndarray, nest: bool, cma
 
     ####################################################################################################################
 
-    #lon_min, lon_max, lat_min, lat_max = _get_bounding_box(nside, footprint, nest)
+    lon_min, lon_max, lat_min, lat_max = _get_bounding_box(nside, footprint, nest)
+
+    print(lon_min, lon_max, lat_min, lat_max)
 
     ####################################################################################################################
 
@@ -65,8 +67,8 @@ def _display(nside: int, footprint: np.ndarray, sky: np.ndarray, nest: bool, cma
         norm = norm,
         min = v_min,
         max = v_max,
-        #lonra = [lon_min, lon_max],
-        #latra = [lat_min, lat_max],
+        lonra = [lon_min, lon_max],
+        latra = [lat_min, lat_max],
         flip = 'geo'
     )
 

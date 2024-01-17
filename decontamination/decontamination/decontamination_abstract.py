db40eb1dd209f1b77ea80b8babef2ab4fd96d2b0
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2024-01-17 21:10:32 +0100

    improving 1D correlations

--- decontamination/decontamination/decontamination_abstract.py
+++ decontamination/decontamination/decontamination_abstract.py
@@ -131,7 +131,7 @@ class Decontamination_Abstract(object):
         # ESTIMATE BINNING                                                                                             #
         ################################################################################################################
 
-        tmp_n_bins = np.full(dim, np.int64(10.0 * n_bins * (1.0 + np.log2(vectors.shape[1]))) if temp_n_bins is None else temp_n_bins, np.int64)
+        tmp_n_bins = np.full(dim, np.int64(5.0 * n_bins * (1.0 + np.log2(vectors.shape[1]))) if temp_n_bins is None else temp_n_bins, np.int64)
 
         print(tmp_n_bins)
 

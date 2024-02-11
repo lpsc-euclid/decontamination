5723d6adeb07120dcbadb67bb3825aaed01ab12c
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2024-02-11 23:37:50 +0100

    update file headers

--- decontamination/algo/som_batch.py
+++ decontamination/algo/som_batch.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 ########################################################################################################################
+# author: Jérôme ODIER <jerome.odier@lpsc.in2p3.fr>
+#         Gaël ALGUERO <gael.alguero@lpsc.in2p3.fr>
+#         Juan MACIAS-PEREZ <juan.macias-perez@lpsc.in2p3.fr>
+# license: CeCILL-C
+########################################################################################################################
 
 import gc
 import math
--- decontamination/jit/atomic.py
+++ decontamination/jit/atomic.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+########################################################################################################################
+# author: Jérôme ODIER <jerome.odier@lpsc.in2p3.fr>
+#         Gaël ALGUERO <gael.alguero@lpsc.in2p3.fr>
+#         Juan MACIAS-PEREZ <juan.macias-perez@lpsc.in2p3.fr>
+# license: CeCILL-C
 ########################################################################################################################
 
 import typing
